290c8fe58727ed11dc79472b347d6919453cda10
amutua <albanusmm20@gmail.com>
2025-08-11 22:22:27 +0300

    takes an int min and an int max as parameters. The function should return a slice of ints with all the values between the min and max.

---
+++ appendrange.go
@@ -0,0 +1,23 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+
+	var result []int // nil slice to start with
+
+	for i := min; i < max; i++ {
+		result = append(result, i)
+	}
+
+	return result
+}
+
+// takes an int min and an int max as parameters. The function should return a slice of ints with all the values between the min and max.
+
+// Min is included, and max is excluded.
+
+// If min is greater than or equal to max, a nil slice is returned.
+
+// make is not allowed.
