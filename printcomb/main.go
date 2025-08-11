dc575f67bc9daad86d1b613264990e891fa67a49
amutua <albanusmm20@gmail.com>
2025-08-11 21:33:33 +0300

    prints, in ascending order and on a single line: all unique combinations of three different digits

--- printcomb/main.go
+++ printcomb/main.go
@@ -18,3 +18,6 @@ func main() {
 		}
 	}
 }
+// Write a function that prints, in ascending order and on a single line: all unique combinations of three different digits so that, the first digit is lower than the second, and the second is lower than the third.
+
+// These combinations are separated by a comma and a space.
