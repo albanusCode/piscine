a00b33d3fce13b8d18aad441fc7f87f444d6e094
amutua <albanusmm20@gmail.com>
2025-08-12 04:38:30 +0300

    hashcode

--- hashcode.go
+++ hashcode.go
@@ -1,12 +1,30 @@
-func HashCode(s string) int {
-	hash := 0
-	p := 31
-	mod := 1_000_000_007 // big prime
-	for i := 0; i < len(s); i++ {
-		hash = (hash*p + int(s[i])) % mod
+package main
+
+func HashCode(dec string) string {
+	size := len(dec)
+	result := ""
+
+	for i := 0; i < size; i++ {
+		val := (int(dec[i]) + size) % 127
+		if val < 33 { // make sure it's printable
+			val += 33
+		}
+		result += string(rune(val))
 	}
-	return hash
+
+	return result
 }
+
+
+// func HashCode(s string) int {
+// 	hash := 0
+// 	p := 31
+// 	mod := 1_000_000_007 // big prime
+// 	for i := 0; i < len(s); i++ {
+// 		hash = (hash*p + int(s[i])) % mod
+// 	}
+// 	return hash
+// }
 
 
 // package piscine
