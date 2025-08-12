76cb2cd5c8835f7759d02ec59ae5595dd3298a6c
amutua <albanusmm20@gmail.com>
2025-08-12 04:50:25 +0300

    printable ascii

---
+++ isprintable.go
@@ -0,0 +1,9 @@
+func IsPrintable(s string) bool {
+	for _, r := range s {
+		// Printable ASCII range is from 32 (' ') to 126 ('~')
+		if r < 32 || r > 126 {
+			return false
+		}
+	}
+	return true
+}
