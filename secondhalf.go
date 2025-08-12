9569e5c61b11755d95b32320f2d96e401b3f8970
amutua <albanusmm20@gmail.com>
2025-08-12 05:39:40 +0300

    second half of a slice

---
+++ secondhalf.go
@@ -0,0 +1,14 @@
+package piscine
+
+func SecondHalf(slice []int) []int {
+    // find the starting index of the second half
+    start := len(slice) / 2
+
+    // if odd length, include the middle element in the second half
+    if len(slice)%2 != 0 {
+        start = len(slice) / 2
+    }
+
+    // return slice starting from 'start'
+    return slice[start:]
+}
