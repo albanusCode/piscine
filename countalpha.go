2504c45f4f09afbda209e0484f63b974e8e4bc6b
amutua <albanusmm20@gmail.com>
2025-08-18 11:38:07 +0300

    counts the no of alphabets in a string.

---
+++ countalpha.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CountAlpha(s string) int {
+	count := 0
+	for _, ch := range s {
+		if ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z'{
+			count ++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+}
