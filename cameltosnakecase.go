ad898d5057c740a8948769d1a0cb716d89292be6
amutua <albanusmm20@gmail.com>
2025-08-19 03:34:53 +0300

    camelcase to snakecase

---
+++ cameltosnakecase.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CamelToSnakeCase(s string) string {
+	length := len(s)
+	if length < 1 {
+		return ""
+	}
+
+	for i := 0; i < length; i++ {
+		if s[length - 1] >= 'A' && s[length - 1] <= 'Z' {
+			return s //if it ends with capital
+		}
+		if s[i] >= 'A' && s[i] <= 'Z' && s[i+1] >= 'A' && s[i+1] <= 'Z'{
+			return s //it has two consecutive
+		}
+		if s[i] < 'A' && s[i] > 'Z' || s[i] < 'a' && s[i] > 'a' {
+			return s
+		}
+	}
+	result := ""
+	for i := 0; i < length; i++ {
+		if s[i] >= 'A' && s[i] <= 'Z' {
+			if i != 0 {
+				result += "_"
+			}
+			result += string(s[i] + 32)
+		}else{
+			result += string(s[i])
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(CamelToSnakeCase("HelloWorld"))         // Hello_World
+	fmt.Println(CamelToSnakeCase("helloWorld"))         // hello_World
+	fmt.Println(CamelToSnakeCase("camelCase"))          // camel_Case
+	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))   // CAMELtoSnackCASE
+	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))   // camel_To_Snake_Case
+	fmt.Println(CamelToSnakeCase("hey2"))               // hey2
+}
+
