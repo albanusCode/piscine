34f3434a3ae43568868483b47fd92c5fe328d471
amutua <albanusmm20@gmail.com>
2025-08-23 20:48:23 +0300

    addprimesum

--- atoi.go
+++ atoi.go
@@ -50,3 +50,63 @@ func atoi(str string) int {
 	return number
 }
 
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+// 	sum := 0
+
+// 	args := os.Args[1]
+// 	if len(os.Args) > 1 {
+// 		fmt.Println(0)
+// 		return
+// 	}
+// 	n := atoi(args)
+// 	for i := 2; i <= n; i++ {
+// 		isPrime := true
+// 		for j := 2; j*j <= i; j++ {
+// 			if i%j == 0 {
+// 				isPrime = false
+// 				break
+// 			}
+// 		}
+// 		if isPrime {
+// 			sum += i
+// 		}
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// func atoi(str string) int {
+// 	if len(str) != 1 {
+// 		return 0
+// 	}
+
+// 	isNeg := false
+// 	isPos := false
+// 	number := 0
+// 	for _, ch := range str {
+// 		if ch == '-' {
+// 			isNeg = true
+// 		}else if ch == '+' {
+// 			isPos = true
+// 		}
+		
+// 		if ch >= '0' && ch <= '9' {
+// 			digit := ch - '0'
+// 			number = (number * 10) + int(digit)
+// 		}
+// 		if isNeg {
+// 			return number * -1
+// 		}else if isPos {
+// 			return number * 1
+// 		}
+// 	}
+// 	return number
+// }
+
